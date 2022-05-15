b1b54988524d910dc6e28b213d9947575b6a172a
Birgit Kratz <mail@birgitkratz.de>
2022-05-16 00:32:04 +0200

    add @SpringBootTest for BookRestController

---
+++ src/test/java/de/workshops/bookshelf/BookRestControllerTest.java
@@ -0,0 +1,26 @@
+package de.workshops.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class BookRestControllerTest {
+
+    @Autowired
+    BookRestController bookRestController;
+
+
+    @Test
+    void getAllBooks() {
+        final var response = bookRestController.getAllBooks();
+        final var books = response.getBody();
+
+        assertEquals(3, books.size());
+
+        assertThat(books).hasSize(2);
+    }
+}
