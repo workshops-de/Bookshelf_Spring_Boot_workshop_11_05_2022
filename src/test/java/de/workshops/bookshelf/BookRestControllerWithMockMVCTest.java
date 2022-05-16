d6be9120782ad7a266ab674bb5f8b35d25b42a34
Birgit Kratz <mail@birgitkratz.de>
2022-05-16 20:48:31 +0200

    @SpringBootTest vs TestSlice @WebMvcTest

--- src/test/java/de/workshops/bookshelf/BookRestControllerWithMockMVCTest.java
+++ src/test/java/de/workshops/bookshelf/BookRestControllerWithMockMVCTest.java
@@ -4,19 +4,22 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class BookRestControllerWithAutoconfigureMockMVCTest {
+@WebMvcTest
+class BookRestControllerWithMockMVCTest {
+
+    @MockBean
+    BookService bookService;
 
     @Autowired
     MockMvc mockMvc;
@@ -26,17 +29,16 @@ class BookRestControllerWithAutoconfigureMockMVCTest {
 
     @Test
     void getAllBooks() throws Exception {
+        when(bookService.getAllBooks()).thenReturn(List.of(new Book()));
+
         final var mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/books"))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        final var jsonString = mvcResult.getResponse().getContentAsString();
+        final var contentAsString = mvcResult.getResponse().getContentAsString();
 
-        List<Book> books = mapper.readValue(jsonString, new TypeReference<>(){});
+        List<Book> books = mapper.readValue(contentAsString, new TypeReference<>() {});
 
-        assertThat(books)
-                .hasSize(3)
-                .first()
-                .hasFieldOrPropertyWithValue("author", "Erich Gamma");
+        assertThat(books).hasSize(1);
     }
 }
